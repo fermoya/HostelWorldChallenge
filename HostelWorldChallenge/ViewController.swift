39f28d09fa4dbf2ffb3fe4540d97625303c8420f
fermoya <fmdr.ct@gmail.com>
2019-08-28 09:51:44 +0100

    Initial commit

---
+++ HostelWorldChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HostelWorldChallenge
+//
+//  Created by Fernando Moya de Rivas on 28/08/2019.
+//  Copyright © 2019 Fernando Moya de Rivas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
